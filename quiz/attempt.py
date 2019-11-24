ec6f5ea0391b6b04851ff20864177dd06897088d
Bryan <ogingabrian2017@gmail.com>
2019-11-24 22:46:40 +0300

    Initial commit

---
+++ quiz/attempt.py
@@ -0,0 +1,37 @@
+
+
+def icorectTrials():
+     print("You failed the guess game !! ")
+
+def PrintMessage():
+    attempt = 0
+
+    while attempt < 3:
+        userInput = int(input("Enter a number : "))
+        attempt += 1
+
+        if userInput == 9:
+            print("Congraculations you win the game")
+            break
+
+    else:
+        icorectTrials()
+
+    
+# function call
+PrintMessage()
+    
+
+
+
+
+
+
+
+        
+       
+        
+       
+
+
+
